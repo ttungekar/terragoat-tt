d86651c2ae9b883698e34d5cc78f48341dbe9b2b
ttungekar <118484314+ttungekar@users.noreply.github.com>
2022-12-15 16:43:23 -0800

    Create newgcs.tf

---
+++ terraform/simple_instance/newgcs.tf
@@ -0,0 +1,5 @@
+resource "google_storage_bucket_iam_binding" "allow_public_read" {
+  bucket  = google_storage_bucket.terragoat_website.id
+  members = ["allUsers"]
+  role    = "roles/storage.objectViewer"
+}
